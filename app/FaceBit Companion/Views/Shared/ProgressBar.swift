4d5acd519c3830eb5606dba52c8edff7e903ec54
Blaine Rothroc <blaine.rothrock@protonmail.com>
2022-01-16 15:13:37 -0600

    add license notices

--- app/FaceBit Companion/Views/Shared/ProgressBar.swift
+++ app/FaceBit Companion/Views/Shared/ProgressBar.swift
@@ -4,6 +4,19 @@
 //
 //  Created by blaine on 2/14/21.
 //
+//  @copyright Copyright (c) 2022 Ka Moamoa
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 3 of the license.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // Credit: https://www.simpleswiftguide.com/how-to-build-linear-progress-bar-in-swiftui/
 
